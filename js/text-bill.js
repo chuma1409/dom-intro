46d816c708d260980fbc865973836befc07db661
chuma1409 <chumagushman14@gmail.com>
2020-05-03 14:04:55 +0200

    dom changes

--- js/text-bill.js
+++ js/text-bill.js
@@ -1,12 +1,46 @@
-// get a reference to the textbox where the bill type is to be entered
 
-//get a reference to the add button
-
-//create a variable that will keep track of the total bill
-
-//add an event listener for when the add button is pressed
-
-//in the event listener check if the value in the bill type textbox is 'sms' or 'call'
-// * add the appropriate value to the running total
-// * add nothing for invalid values that is not 'call' or 'sms'.
-// * display the latest total on the screen
+ const textTotalAddBtn = document.querySelector(".addToBillBtn");
+ const billTypeText = document.querySelector(".billTypeText");
+ const callTotalElem = document.querySelector(".callTotalOne");
+ const smsTotalElem = document.querySelector(".smsTotalOne");
+ const totalCost = document.querySelector(".totalOne");
+ 
+ 
+     var billTotal =0;
+   var callsTotal =0;
+   var smsTotal =0;
+ 
+ function textTotalAddBtnClicked(){
+ 
+     // get the value entered in the billType textfield
+     var billItems = billTypeText.value
+     // update the correct total
+   var billItem = billItems.trim();
+     if (billItem === "call"){
+       billTotal += 2.75;
+         callsTotal += 2.75;
+     }
+     else if (billItem === "sms"){
+       billTotal += 0.75
+         smsTotal += 0.75;
+     }
+     //update the totals that is displayed on the sc.
+ 
+   totalAddBtnClicked()
+ }
+ // alert(billTotal)
+ 
+ function totalAddBtnClicked(){
+       callTotalElem.innerHTML = callsTotal.toFixed(2);
+       smsTotalElem.innerHTML = smsTotal.toFixed(2);
+       totalCost.innerHTML = billTotal.toFixed(2); 
+   
+       if (billTotal >= 50){
+        // adding the danger class will make the text red
+        totalCost.classList.add("danger");
+    }
+    else if (billTotal >= 30){
+        totalCost.classList.add("warning");
+    }
+}
+ textTotalAddBtn.addEventListener("click", textTotalAddBtnClicked);
