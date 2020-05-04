89e1e56803f5b7ba686c61e705e00637981dd4e4
chuma1409 <chumagushman14@gmail.com>
2020-05-04 13:47:19 +0200

    radio-bill changes

--- js/radio-bill.js
+++ js/radio-bill.js
@@ -1,12 +1,49 @@
-// get a reference to the sms or call radio buttons
+const textTotalAddBtn = document.querySelector(".radioBillAddBtn");
+const billTypeText = document.querySelector(".billItemTypeRadio");
+const callTotalElem = document.querySelector(".callTotalTwo");
+const smsTotalElem = document.querySelector(".smsTotalTwo");
+const totalCost = document.querySelector(".totalTwo");
+alert(textTotalAddBtn)
+  var billTotal =0;
+  var callsTotal =0;
+  var smsTotal =0;
 
-//get a reference to the add button
+function textTotalAddBtnClicked(){
+  alert("chuma")
+var checkedRadioBtn = document.querySelector("input[name='billItemType']:checked");
+if (checkedRadioBtn){
+   var billItemType = checkedRadioBtn.value
+   // billItemType will be 'call' or 'sms'
+}
+    // get the value entered in the billType textfield
+   
+    // update the correct total
+  var billItem = billItemType.trim();
+    if (billItem === "call"){
+      billTotal += 2.75;
+        callsTotal += 2.75;
+    }
+    else if (billItem === "sms"){
+      billTotal += 0.75
+        smsTotal += 0.75;
+    }
+    //update the totals that is displayed on the sc.
 
-//create a variable that will keep track of the total bill
+  totalAddBtnClicked()
+}
+// alert(billTotal)
 
-//add an event listener for when the add button is pressed
-
-//in the event listener get the value from the billItemTypeRadio radio buttons
-// * add the appropriate value to the running total
-// * add nothing for invalid values that is not 'call' or 'sms'.
-// * display the latest total on the screen
+function totalAddBtnClicked(){
+      callTotalElem.innerHTML = callsTotal.toFixed(2);
+      smsTotalElem.innerHTML = smsTotal.toFixed(2);
+      totalCost.innerHTML = billTotal.toFixed(2); 
+  
+      if (billTotal >= 50){
+       // adding the danger class will make the text red
+       totalCost.classList.add("danger");
+   }
+   else if (billTotal >= 30){
+       totalCost.classList.add("warning");
+   }
+}
+textTotalAddBtn.addEventListener("click", textTotalAddBtnClicked);
